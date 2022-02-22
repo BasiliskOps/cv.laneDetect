e2aed8bda77353c02499ccb0fa2c510ddc85e141
BasiliskOps <noro.nero@gmail.com>
2022-02-21 20:21:35 -0700

    if statement with Boolean to notify user if video stream failed

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture('/Users/basilisk/Data_Science/Capstone/cv.laneDetect/driving_frame.mp4')
+
+if (cap.isOpened() == False):
+    print("Video stream failed to initialize")
